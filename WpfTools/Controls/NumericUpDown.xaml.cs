ec00dd8b34445c05e33431f6996e36fe78f61481
Vince <innatepirate@gmail.com>
2013-10-16 17:49:07 -0500

    initial

---
+++ WpfTools/Controls/NumericUpDown.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTools.Controls
+{
+    /// <summary>
+    /// Interaction logic for NumericUpDown.xaml
+    /// </summary>
+    public partial class NumericUpDown : UserControl
+    {
+        public enum NumberType
+        {
+            Integer, Double, Decimal, UnsignedInt
+        }
+        public struct Number
+        {
+            public NumberType Type { get; set; }
+            public object Value { get; set; }
+
+            public T Cast<T>()
+            {
+                if (Value is T)
+                    return (T)Value;
+                return default(T);
+            }
+
+            public double GetDouble()
+            {
+                return Cast<double>();
+            }
+
+            public decimal GetDecimal()
+            {
+                return Cast<decimal>();
+            }
+
+            public int GetInt()
+            {
+                return Cast<int>();
+            }
+
+            public uint GetUInt()
+            {
+                return Cast<uint>();
+            }
+
+            public static implicit operator double(Number n)
+            {
+                return n.GetDouble();
+            }
+
+            public static implicit operator decimal(Number n)
+            {
+                return n.GetDecimal();
+            }
+
+            public static implicit operator int(Number n)
+            {
+                return n.GetInt();
+            }
+
+            public static implicit operator uint(Number n)
+            {
+                return n.GetUInt();
+            }
+
+            public static implicit operator Number(decimal d)
+            {
+                return new Number() { Type = NumberType.Decimal, Value = d };
+            }
+            public static implicit operator Number(double d)
+            {
+                return new Number() { Type = NumberType.Double, Value = d };
+            }
+            public static implicit operator Number(int d)
+            {
+                return new Number() { Type = NumberType.Integer, Value = d };
+            }
+
+            public static implicit operator Number(uint d)
+            {
+                return new Number() { Type = NumberType.UnsignedInt, Value = d };
+            }
+        }
+
+        [Browsable(true)]
+        public NumberType ValueType { get; set; }
+
+        [Browsable(true)]
+        public Number Minimum { get; set; }
+        [Browsable(true)]
+        public Number Maximum { get; set; }
+        [Browsable(true)]
+        public Number Value { get; set; }
+        [Browsable(true)]
+        public string TextHint
+        {
+            get { return txtBox.TextHint; }
+            set { txtBox.TextHint = value; }
+        }
+
+        public NumericUpDown()
+        {
+            InitializeComponent();
+            Loaded += NumericUpDown_Loaded;
+        }
+
+        void NumericUpDown_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfTools/Controls/TextBoxExtended.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTools.Controls
+{
+    /// <summary>
+    /// Interaction logic for TextBoxExtended.xaml
+    /// </summary>
+    public partial class TextBoxExtended : UserControl
+    {
+        [Browsable(true), Bindable(true)]
+        public string TextHint { get; set; }
+        [Browsable(true), Bindable(true)]
+        public string RegexValidator { get; set; }
+        [Browsable(true), Bindable(true)]
+        public Brush HintBrush { get; set; }
+
+        private Regex _validator;
+
+        private Regex TextValidator
+        {
+            get { return _validator = _validator ?? new Regex(RegexValidator); }
+        }
+        private bool _usesHint
+        {
+            get { return !string.IsNullOrWhiteSpace(TextHint); }
+        }
+
+        private bool _usesRegex
+        {
+            get { return !string.IsNullOrWhiteSpace(RegexValidator); }
+        }
+        private bool _hintShown { get; set; }
+
+        private string _text
+        {
+            get { return txtBox.Text; } 
+            set { txtBox.Text = value; }
+        }
+
+        public TextBoxExtended()
+        {
+            InitializeComponent();
+            Loaded += TextBoxExtended_Loaded;
+        }
+
+        void TextBoxExtended_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (_usesHint)
+                ShowHint();
+
+            if (_usesHint || _usesRegex)
+            {
+                txtBox.KeyDown += txtBox_KeyDown;
+                txtBox.KeyUp += txtBox_KeyUp;
+                txtBox.GotFocus += txtBox_GotFocus;
+                txtBox.LostFocus += txtBox_LostFocus;
+            }
+        }
+
+        void txtBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            ShowHint();
+        }
+
+        void txtBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ShowHint();
+        }
+
+        void txtBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            ShowHint();
+        }
+
+        private readonly Key[] _ignoredKeys = new[]
+        {
+            Key.Escape,
+            Key.LeftShift,
+            Key.RightShift,
+            Key.LeftCtrl,
+            Key.RightCtrl,
+            Key.LeftAlt,
+            Key.RightAlt,
+            Key.Enter,
+            Key.End,
+            Key.Home,
+            Key.PageDown,
+            Key.PageUp,
+            Key.Back
+        };
+        void txtBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!_ignoredKeys.Contains(e.Key))
+                HideHint();
+            else
+                ShowHint();
+        }
+        
+        private void ShowHint()
+        {
+            if (_hintShown) return;
+            if (!string.IsNullOrWhiteSpace(_text) && _text != TextHint)
+                return;
+            _text = TextHint;
+            HintBrush = HintBrush ?? Brushes.DarkSlateGray;
+            txtBox.Foreground = HintBrush;
+            _hintShown = true;
+        }
+
+        private void HideHint()
+        {
+            if (!_hintShown) return;
+            if (_text != TextHint) return;
+            _text = "";
+            txtBox.Foreground = Foreground;
+            _hintShown = false;
+        }
+    }
+}
